2e5ab45354ef42d8ea2d200e5aee579fd965f6ef
ayananshTheCoder <78261948+ayananshTheCoder@users.noreply.github.com>
2021-08-25 17:53:06 +0530

    Add files via upload
    Hey guys! Here is a basic example of the canvas function!

---
+++ clown-nose-filter-web-app/main.js
@@ -0,0 +1,48 @@
+var noseX = 0;
+var noseY = 0;
+
+function preload()
+{
+    clown_nose = loadImage('https://i.postimg.cc/kX5wpnZk/580b57fbd9996e24bc43bed5.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(900,600);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet has been initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX-13, noseY-13, 30, 30);
+}
+
+function take_snapshot()
+{
+    save('myFitlerImage.png');
+}
+
